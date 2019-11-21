049c90466e042d6d5bdc42d63a785d5cd1d78f3a
Jaxon Burningham <jaxonburn@gmail.com>
2019-11-21 10:15:07 -0700

    Major additions

--- action.js
+++ action.js
@@ -34,16 +34,17 @@ function selectCourse(value){
     let selectImg = golf.courses[value].image;
     let selectName = golf.courses[value].name;
     getCourseInfo(selectId);
-    setTimeout(function(){ printTees(selectName, selectImg)}, 200);
+    setTimeout(function(){ printTees(selectName, selectImg)}, 220);
     
 }
 
 function printTees(Name, Img){
   console.log(golfInfo);
+  let golfName = Name;
   let teeLength = golfInfo.data.holes[0].teeBoxes.length; 
   $(".golfname").html(Name)
-  $(".select").html(`<img src="${Img}"></img><select class="tees" onchange="loadHoles(this.value)">
-  <option value="">Select Difficulty</option>
+  $(".select").html(`<img src="${Img}"><select class="tees" onchange="loadHoles(this.value, '${Name}')">
+  <option>Select Difficulty</option>
   </select>`);
   for (i = 0; i < teeLength; i++){
     $(".tees").append(`
@@ -53,14 +54,69 @@ function printTees(Name, Img){
   
 }
 
-function loadHoles(value) {
-  golfInfo.data.holes.length += 1;
-  $(".pickcourse").fadeOut();
-  $("#head").append(`<span></span>`)
-  $("#holecontain").css({"display": "flex", "text-align": "center", "justify-content": "space-evenly"});
-  for(let i = 0;i < golfInfo.data.holes.length; i++){
-    $("#holecontain").append(`<div class="hole" id="hole${i}">${i}</div>`)
-  }
+function loadHoles(value, Name) {
+  $(".pickcourse").fadeOut(200);
+  let difficulty;
+  let totalyards = 0;
+  let totalpar = 0;
+  let totalhcp = 0;
+  let inyards = 0;
+  let outyards = 0;
+  let inpar = 0;
+  let outpar = 0;
+  let outhcp = 0;
+  let inhcp = 0;
+  let parcontain = $('#parcontain');
+  let hcpcontain = $('#hcpcontain');
+
+  setTimeout(function() {
+    $("#head").show();
+    $("#head").append(`<span>${Name}</span>`)
+    $("#holecontain").css({"display": "flex", "text-align": "center", "justify-content": "space-evenly"});
+    $("#yardcontain").css({"display": "flex", "text-align": "center", "justify-content": "space-evenly"});
+    $("#addPlayer").css({"display": "flex", "text-align": "center", "justify-content": "space-evenly"});
+    hcpcontain.css({"display": "flex", "text-align": "center", "justify-content": "space-evenly"});
+    parcontain.css({"display": "flex", "text-align": "center", "justify-content": "space-evenly"});
+    document.getElementById("playerbutton").classList.add("addPlayer");
+    document.getElementById("contain").style.justifyContent = "flex-start";
+    for(let i = 0;i < golfInfo.data.holes.length; i++){
+      $("#holecontain").append(`<div class="hole" id="hole${i}">${i + 1}</div>`)
+      for(j = 0; j < golfInfo.data.holes[i].teeBoxes.length; j++){
+        if(golfInfo.data.holes[i].teeBoxes[j].teeType === value){
+          if(i < 9) {
+            outyards += golfInfo.data.holes[i].teeBoxes[j].yards;
+            outpar += golfInfo.data.holes[i].teeBoxes[j].par;
+            outhcp += golfInfo.data.holes[i].teeBoxes[j].hcp;
+          }
+          if(i >= 9) {
+            inyards += golfInfo.data.holes[i].teeBoxes[j].yards;
+            inpar += golfInfo.data.holes[i].teeBoxes[j].par;
+            inhcp += golfInfo.data.holes[i].teeBoxes[j].hcp;
+          }
+          totalhcp += golfInfo.data.holes[i].teeBoxes[j].hcp;
+          totalpar += golfInfo.data.holes[i].teeBoxes[j].par;
+          totalyards += golfInfo.data.holes[i].teeBoxes[j].yards
+          $("#yardcontain").append(`<div class="hole" id="yard${i}">${golfInfo.data.holes[i].teeBoxes[j].yards}</div>`)
+          parcontain.append(`<div class="hole" id="par${i}">${golfInfo.data.holes[i].teeBoxes[j].par}</div>`)
+          hcpcontain.append(`<div class="hole" id="hcp${i}">${golfInfo.data.holes[i].teeBoxes[j].hcp}</div>`)
+         
+        }
+    }  
+
+  }    
+  document.getElementById("par8").outerHTML += `<div class="in">${outpar}</div>`
+  document.getElementById("par17").outerHTML +=  `<div id="parout" class="in">${inpar}</div>`
+  document.getElementById("parout").outerHTML += `<div class="Total in">${totalpar}</div>`
+      document.getElementById("hcp8").outerHTML += `<div class="in">${outhcp}</div>`
+      document.getElementById("hcp17").outerHTML +=  `<div id="hcpout" class="in">${inhcp}</div>`
+      document.getElementById("hcpout").outerHTML += `<div class="Total in">${totalhcp}</div>`
+    document.getElementById("yard8").outerHTML += `<div id="yardin" class="in">${outyards}</div>`
+    document.getElementById("yard17").outerHTML +=  `<div id="yardout" class="in">${inyards}</div>`
+    document.getElementById("yardout").outerHTML += `<div id="totalyards" class="Total in">${totalyards}</div>`
+    document.getElementById("hole8").outerHTML += `<div class="in">Out</div>`
+    document.getElementById("hole17").outerHTML +=  `<div id="out" class="in">In</div>`
+    document.getElementById("out").outerHTML += `<div class="Total in">Total</div>`
+  }, 210)
   
 }
 
@@ -88,3 +144,7 @@ function getCourseInfo(Id) {
 getCourseInfo().then(course => {
   
 })
+
+function addPlayer(){
+  
+}
